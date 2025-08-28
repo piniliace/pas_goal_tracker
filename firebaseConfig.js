717e38d93ad26edcf6366b63f451bd5c21f732f3
piniliace <piniliacesmith@gmail.com>
2025-08-28 11:43:36 +0800

    firebase configuration

---
+++ firebaseConfig.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBftWVZW8Hihl1fOwKL7eft1nRRJS7BFAo",
+  authDomain: "pas-goal-tracker.firebaseapp.com",
+  projectId: "pas-goal-tracker",
+  storageBucket: "pas-goal-tracker.firebasestorage.app",
+  messagingSenderId: "612487363894",
+  appId: "1:612487363894:web:af235a818ff7be6f33c797"
+};
+const app = initializeApp(firebaseConfig);
